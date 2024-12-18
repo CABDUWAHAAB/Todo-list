d7e27c63cf41d36643147e7d269c2007157fd652
abdi <abdi@pluxbox.com>
2024-12-18 23:18:55 +0100

    postCard

--- todo-list-frontend/src/components/TodoList.tsx
+++ todo-list-frontend/src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_TODOS } from '../graphql/queries';
+import '../assets/scss/blogCard.scss';
 
 const TodoList: React.FC = () => {
   const { data, loading, error } = useQuery(GET_TODOS);
@@ -9,20 +10,22 @@ const TodoList: React.FC = () => {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="p-6 bg-white rounded-lg shadow">
+    <article className='blogCards'>
       <h1 className="text-xl font-bold mb-4">Todo List</h1>
-      <ul>
-        {data.todos.map((todo: {id: string, title: string, description: string, completed: boolean}) => (
-          <li key={todo.id} className="mb-4">
-            <h2 className="text-lg font-semibold">{todo.title}</h2>
-            <p className="text-gray-600">{todo.description}</p>
-            <p className={`text-sm ${todo.completed ? 'text-green-500' : 'text-red-500'}`}>
-              {todo.completed ? 'Completed' : 'Incomplete'}
-            </p>
-          </li>
-        ))}
-      </ul>
+      <div className="blogCards__Box">
+        <ul className='blogCards__Card'>
+          {data.todos.map((todo: {id: string, title: string, description: string, completed: boolean}) => (
+            <li key={todo.id} className="mb-4">
+              <h2 className="text-lg font-semibold">{todo.title}</h2>
+              <p className="text-gray-600">{todo.description}</p>
+              <p className={`text-sm ${todo.completed ? 'text-green-500' : 'text-red-500'}`}>
+                {todo.completed ? 'Completed' : 'Incomplete'}
+              </p>
+            </li>
+          ))}
+        </ul>
     </div>
+    </article>
   );
 };
 
--- todo-list-frontend/src/pages/Home.tsx
+++ todo-list-frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TodoList from '../components/TodoList';
 import TodoForm from '../components/TodoForm';
-import "./Home.scss";
+import "../assets/scss/Home.scss";
 
 const Home: React.FC = () => {
   return (
